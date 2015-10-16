a1f4cc6680c87b216136cacafda880c073b40394
maks-ivanov <m0597@berkeley.edu>
2015-10-16 12:55:03 -0700

    done

--- coinworth.py
+++ coinworth.py
@@ -1,15 +1,21 @@
 import datetime
 import requests
+#
 import simplejson as json
+#
 import sqlite3
 import schedule
 import time
+#
 from wtforms import Form, BooleanField, TextField, PasswordField, validators, FloatField, RadioField, SelectField
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+#
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 
-################################################
+#######################################################################
 
 DATABASE = '/tmp/cw.sqlite'
 DEBUG = True
@@ -24,11 +30,9 @@ app.config.from_envvar('CW_SETTINGS', silent=True)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-if __name__ == '__main__':
-    app.run()
 
 
-################################################
+#######################################################################
 
 @app.before_request
 def before_request():
@@ -40,7 +44,7 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-################################################
+#######################################################################
 
 @app.route('/')
 def show_entries():
@@ -58,8 +62,31 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
-################################################
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+#######################################################################
+
+if __name__ == '__main__':
+    app.run()
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=4, max=25)])
@@ -68,3 +95,9 @@ class RegistrationForm(Form):
     operator= SelectField('is', choices = ['worth at least', 'worth less than', 'devalued by 5%', 'grown by 5%'])
     usd_val=FloatField('$', [[validators.Length(min=0, max=5)]])
     accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+
+
+#######################################################################
+
+
+
