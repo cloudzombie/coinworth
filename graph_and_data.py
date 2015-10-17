f4a4eca92db49c5b57580c0444186aaa79ad3561
lukeanders70 <luke.larson@berkeley.edu>
2015-10-16 21:56:33 -0700

    made it work

--- graph_and_data.py
+++ graph_and_data.py
@@ -12,8 +12,8 @@ def loop(i):
 		ab.create_price_table('test_table2.sqlite')
 	ab.update_prices((var0,var1,var2,var3,var4,var5,var6,var7,var8),'test_table2.sqlite')
 
-	x_data = ab.get_column('time', 'data', 'test_table2.sqlite')
-	y_data = ab.get_column('last', 'data', 'test_table2.sqlite')
+	x_data = ab.get_column('time', 'prices', 'test_table2.sqlite')
+	y_data = ab.get_column('last', 'prices', 'test_table2.sqlite')
 
 	'''some magic that makes the plot and uploads it to the plotly hosting site'''
 	trace0 = Scatter(x=x_data, y=y_data)
@@ -24,5 +24,5 @@ def loop(i):
 	counter (for use in the ID parapater of each row in the data set)'''
 	sleep(10)
 
-for x in range(11):
+for x in range(1,11):
 	loop(x)
